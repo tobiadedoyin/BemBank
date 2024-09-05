024b8d41e5359692c4c577a0743c873bec56d6cc
Tobi Oladele <oladeletobiadedoyin@gmail.com>
2024-09-05 15:39:17 +0100

    new setup

---
+++ src/main/java/com/bemfot/bem_bank/BemBankApplication.java
@@ -0,0 +1,13 @@
+package com.bemfot.bem_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BemBankApplication.class, args);
+    }
+
+}
